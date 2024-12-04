275114a1e03dac55a83dd7f2bc0c78069353bcee
Sweep <joshuapatrickchiu76@gmail.com>
2024-12-04 21:32:37 +0800

    feat: added accounts

---
+++ src/data/account.js
@@ -0,0 +1,8 @@
+const accounts = [
+    {id: 1, name: 'Joshua Chiu', balance: 1500},
+    {id: 2, name: 'John Doe', balance: 2000},
+    {id: 3, name: 'Jane Doe', balance: 2500},
+    {id: 4, name: 'Jenny Doe', balance: 3000},
+]
+
+export default accounts;
