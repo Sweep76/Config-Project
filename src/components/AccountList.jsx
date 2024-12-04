32a00778cc47e891f7884c1017ca04eda1edf7a4
Sweep <joshuapatrickchiu76@gmail.com>
2024-12-04 21:50:12 +0800

    feat: modified accountlist

--- src/components/AccountList.jsx
+++ src/components/AccountList.jsx
@@ -1,13 +1,37 @@
 // src/components/AccountList.jsx
-import React from 'react';
+import React, { useState } from 'react';
 import Account from './Account';
-import accounts from '../data/accounts';
+import Deposit from './Deposit';
+import Withdraw from './Withdraw';
+import accountsData from '../data/accounts';
 
 const AccountList = () => {
+  const [accounts, setAccounts] = useState(accountsData);
+
+  const handleDeposit = (id, amount) => {
+    setAccounts((prev) =>
+      prev.map((account) =>
+        account.id === id ? { ...account, balance: account.balance + amount } : account
+      )
+    );
+  };
+
+  const handleWithdraw = (id, amount) => {
+    setAccounts((prev) =>
+      prev.map((account) =>
+        account.id === id ? { ...account, balance: account.balance - amount } : account
+      )
+    );
+  };
+
   return (
     <div className="account-list">
-      {accounts.map(account => (
-        <Account key={account.id} account={account} />
+      {accounts.map((account) => (
+        <div key={account.id}>
+          <Account account={account} />
+          <Deposit account={account} onDeposit={handleDeposit} />
+          <Withdraw account={account} onWithdraw={handleWithdraw} />
+        </div>
       ))}
     </div>
   );
